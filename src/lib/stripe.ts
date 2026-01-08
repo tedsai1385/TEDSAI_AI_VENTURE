1e105df18b3ad04ee5daa3ff49f9ff17867f18c2
tedsai1385 <tedsai1385@gmail.com>
2026-01-08 09:56:27 +0100

    Downgrade Stripe API version to match installed types

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,6 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2023-10-16',
+    apiVersion: '2023-08-16',
     typescript: true,
 });
